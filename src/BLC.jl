3869f2b6adce37daa0af06ee4d77aa575d0044e2
Philipp Gabler <phips.gabler@gmail.com>
2017-04-04 14:43:24 +0200

    Try to fix dependencies

--- src/BLC.jl
+++ src/BLC.jl
@@ -1,7 +1,6 @@
 module BLC
 
 using Lazy
-using PEGParser
 
 import Base
 
@@ -200,30 +199,30 @@ encode(abs::IAbs) = "\0\0" * encode(abs.body)
 encode(app::IApp) = "\0\x01" * encode(app.car) * encode(app.cdr)
 
 
-@grammar blc begin
-    start = term{ _1 }
-    term = var | abs | app 
-    var = (+("\x01") + "\0"){ IVar(length(_1.value)) }
-    abs = ("\0\0" + term){ IAbs(_2) }
-    app = ("\0\x01" + term + term){ IApp(_2, _3) }
-end
+# @grammar blc begin
+#     start = term{ _1 }
+#     term = var | abs | app 
+#     var = (+("\x01") + "\0"){ IVar(length(_1.value)) }
+#     abs = ("\0\0" + term){ IAbs(_2) }
+#     app = ("\0\x01" + term + term){ IApp(_2, _3) }
+# end
 
-"""
-Parse a De Bruijn indexed term from its binary representation.
+# """
+# Parse a De Bruijn indexed term from its binary representation.
 
-Returns a `Nullable{IndexedLambda}`, depending on the success of the parsing.
-On error, the function `onerror` is called on the error value of the parser.
-"""
-function decode(bytes::AbstractString, onerror = e -> return)
-    parser_result = parse(blc, bytes)
+# Returns a `Nullable{IndexedLambda}`, depending on the success of the parsing.
+# On error, the function `onerror` is called on the error value of the parser.
+# """
+# function decode(bytes::AbstractString, onerror = e -> return)
+#     parser_result = parse(blc, bytes)
     
-    if parser_result[3] == nothing
-        return Nullable(parser_result[1])
-    else
-        onerror(parser_result[3])
-        return Nullable{IndexedLambda}()
-    end
-end
+#     if parser_result[3] == nothing
+#         return Nullable(parser_result[1])
+#     else
+#         onerror(parser_result[3])
+#         return Nullable{IndexedLambda}()
+#     end
+# end
 
 
 #############################
